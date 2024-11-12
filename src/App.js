f077185d487c0ae41217cd03159d10f0045d6962
4thdraw <4thdraw2@gmail.com>
2024-11-12 11:28:02 +0900

    Ui컴포넌트 공유화하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 
 import { Uimenu } from './commonui/uimenu';
+import './commonui/scss/bgsvg.scss'
 
 function App() {
   return (
     <div className="App">
       <div className='d-flex gap-3'>
-        <Uimenu ><span className='d-block mt-2'>정기배송</span></Uimenu>
-        <Uimenu ><span className='d-block mt-2'>정기배송</span></Uimenu>
+        <Uimenu  className='bg0'><span className='d-block mt-2'>정기배송</span></Uimenu>
+        <Uimenu  className='bg1'><span className='d-block mt-2'>정기배송</span></Uimenu>
         <Uimenu ><span className='d-block mt-2'>정기배송</span></Uimenu>
         <Uimenu ><span className='d-block mt-2'>정기배송</span></Uimenu>
       </div>
