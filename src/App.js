be56b0128a423d26c09993f5d7c61ec1366c534a
4thdraw <4thdraw2@gmail.com>
2024-11-13 11:10:35 +0900

    개략적인 설계

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 
-import { Uimenu } from './commonui/uimenu';
+import { Uimenu, GlobalStyle } from './commonui/uimenu';
 import './commonui/scss/bgsvg.scss'
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle></GlobalStyle>
       <div className='d-flex gap-3'>
         <Uimenu  className='bg0'><span className='d-block mt-2'>정기배송</span></Uimenu>
         <Uimenu  className='bg1'><span className='d-block mt-2'>이벤트</span></Uimenu>
         <Uimenu ><span className='d-block mt-2'>정기배송</span></Uimenu>
         <Uimenu ><span className='d-block mt-2'>정기배송</span></Uimenu>
       </div>
-
     </div>
   );
 }
--- src/commonui/uimenu.js
+++ src/commonui/uimenu.js
@@ -1,34 +1,41 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
-export  const  Uimenu = styled.button`
-      color: #000;
-      leading-trim: both;
-      text-edge: cap;
-      font-family: 'Pretendard';
-      font-size: 16px;
-      font-style: normal;
-      font-weight: 200;
-      line-height: normal;
-
-
-      width: 75px;
-      height: 75px;     
-      background-repeat: no-repeat;    
-      background-position: center;
-      border-radius: 55px;
-      border: 1.2px solid #EEE;
-      background-color: #F6FAF9;
-
-
-       &::before {
-           
-            display:flex;
-            align-items: center;
-            justify-content:center;
-            height:100%;
-      }
+// 태그가 없는 컴포넌트
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary-color: #000;
+    --border-color: #EEE;
+    --background-color: #F6FAF9;
+    --font-family: 'Pretendard';
+    --font-weight-light: 200;
+    --font-size: 16px;
+    --line-height: normal;
+  }
 `;
 
+export const Uimenu = styled.button`
+  color: var(--primary-color);
+  font-family: var(--font-family);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight-light);
+  line-height: var(--line-height);
+  
+  leading-trim: both;
+  text-edge: cap;
 
+  width: 75px;
+  height: 75px;     
+  background-repeat: no-repeat;    
+  background-position: center;
+  border-radius: 55px;
+  border: 1.2px solid var(--border-color);
+  background-color: var(--background-color);
 
+  &::before {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+`;
 
